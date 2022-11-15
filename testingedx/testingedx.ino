3529a1663c3463abb29118be386b17e63d93c396
Jason <jason@localhost.localdomain>
2022-11-15 09:59:47 -0500

    Added UART I/O

--- testingedx/testingedx.ino
+++ testingedx/testingedx.ino
@@ -1,116 +1,94 @@
 /*
-  IMU Capture
-  This example uses the on-board IMU to start reading acceleration and gyroscope
-  data from on-board IMU and prints it to the Serial Monitor for one second
-  when the significant motion is detected.
-  You can also use the Serial Plotter to graph the data.
-  The circuit:
-  - Arduino Nano 33 BLE or Arduino Nano 33 BLE Sense board.
-  Created by Don Coleman, Sandeep Mistry
-  Modified by Dominic Pajak, Sandeep Mistry
-  This example code is in the public domain.
+  Waits for acceleration to go above a threshold, then sends IMU and environmental readings over debug and uart
 */
 
 #include <Arduino_LSM9DS1.h>
 #include <Arduino_LPS22HB.h>
 
-const float accelerationThreshold = 2.5; // threshold of significant in G's
-const int numSamples = 119;
+const float accelerationThreshold = 2.5; // threshold of significant motion in g
+char outBuf[2048];
+float aX, aY, aZ, gX, gY, gZ, mX, mY, mZ;
+float pressure, temperature, altitude;
+bool motion = false;
 
-int samplesRead = numSamples;
-
-void setup() {
+void setup() 
+{
   Serial.begin(9600);
+  Serial1.begin(115200);
   while (!Serial);
 
-  if (!IMU.begin()) {
+  if (!IMU.begin()) 
+  {
     Serial.println("Failed to initialize IMU!");
     while (1);
   }
 
-  if (!BARO.begin()) {
+  if (!BARO.begin()) 
+  {
     Serial.println("Failed to initialize pressure sensor!");
     while (1);
   }
 
-  // print the header
-  Serial.println("aX,aY,aZ,gX,gY,gZ,mX,mY,mZ");
+  // print a csv-ish header
+  Serial.println("aX,aY,aZ,gX,gY,gZ,mX,mY,mZ,p,T,a");
 }
 
-void loop() {
-  float aX, aY, aZ, gX, gY, gZ, mX, mY, mZ;
+void sendReport()
+{
+  sprintf(outBuf, "{\"Accel:\"{\"x\":%.2f,\"y\":%.2f,\"z\":%.2f},\"Mag\":{\"x\":%.2f,\"y\":%.2f,\"z\":%.2f},\"Gyro\":{\"x\":%.2f\"y\":%.2f\":z\":%.2f},\"Pressure\":%.2f,\"Temp\":%.2f,\"Alt\":%.2f}",aX,aY,aZ,gX,gY,gZ,mX,mY,mZ,pressure,temperature,altitude);
+  Serial1.println(outBuf);
+}
 
-  // wait for significant motion
-  while (samplesRead == numSamples) {
-    float pressure = BARO.readPressure();
-    float temperature = BARO.readTemperature();
-    float altitude = 44330 * ( 1 - pow(pressure/101.325, 1/5.255) );
+void loop() 
+{
+  String inString; 
 
-    Serial.print("Pressure = ");
-    Serial.print(pressure);
-    Serial.println(" kPa");
+  if(Serial1.available() > 0)
+  {
+    inString = Serial1.readString();
+    inString.trim();
+    if (inString=="REPORT")
+    {
+      Serial.println("Report requested via UART");
+      sendReport();
+    }
+  }
+  
+  if ((!motion)&&(IMU.accelerationAvailable()))
+  {
+    // read the acceleration data
+    IMU.readAcceleration(aX, aY, aZ);
 
-    Serial.print("Temperature = ");
-    Serial.print(temperature);
-    Serial.println(" C");
+    // sum up the absolutes
+    float aSum = fabs(aX) + fabs(aY) + fabs(aZ);
 
-    Serial.print("Altitude = ");
-    Serial.print(altitude);
-    Serial.println(" m");
-
-    if (IMU.accelerationAvailable()) {
-      // read the acceleration data
-      IMU.readAcceleration(aX, aY, aZ);
-
-      // sum up the absolutes
-      float aSum = fabs(aX) + fabs(aY) + fabs(aZ);
-
-      // check if it's above the threshold
-      if (aSum >= accelerationThreshold) {
-        // reset the sample read count
-        samplesRead = 0;
-        break;
-      }
+    // check if it's above the threshold
+    if (aSum >= accelerationThreshold) 
+    {
+      motion = true;
     }
   }
 
-  // check if the all the required samples have been read since
-  // the last time the significant motion was detected
-  while (samplesRead < numSamples) {
-    // check if both new acceleration and gyroscope data is
-    // available
-    if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable() && IMU.magneticFieldAvailable()) {
-      // read the acceleration and gyroscope data
+  if(motion) 
+  {
+    // Check data available
+    if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable() && IMU.magneticFieldAvailable()) 
+    {
+      pressure = BARO.readPressure();
+      temperature = BARO.readTemperature();
+      altitude = 44330 * ( 1 - pow(pressure/101.325, 1/5.255) );
+
+      // read the acceleration, mag, and gyroscope data
       IMU.readAcceleration(aX, aY, aZ);
       IMU.readGyroscope(gX, gY, gZ);
       IMU.readMagneticField(mX, mY, mZ);
 
-      samplesRead++;
-
-      // print the data in CSV format
-      Serial.print(aX, 3);
-      Serial.print(',');
-      Serial.print(aY, 3);
-      Serial.print(',');
-      Serial.print(aZ, 3);
-      Serial.print(',');
-      Serial.print(gX, 3);
-      Serial.print(',');
-      Serial.print(gY, 3);
-      Serial.print(',');
-      Serial.print(gZ, 3);
-      Serial.print(',');
-      Serial.print(mX, 3);
-      Serial.print(',');
-      Serial.print(mY, 3);
-      Serial.print(',');
-      Serial.print(mZ, 3);
-      Serial.println();
-
-      if (samplesRead == numSamples) {
-        // add an empty line if it's the last sample
-        Serial.println();
-      }
+      // print the data locally
+      sprintf(outBuf, "%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f",aX,aY,aZ,gX,gY,gZ,mX,mY,mZ,pressure,temperature,altitude);
+      Serial.println(outBuf);
+ 
+      sendReport();
+      motion = false;
     }
   }
 }
